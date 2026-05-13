9cfc1dddd44a12361a2363fbefe5b679bc571f6a
borsodiadam <borsodi.dm@gmail.com>
2026-05-13 12:51:24 +0200

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export const metadata: Metadata = {
     siteName: 'Nita Nails',
     images: [
       {
-        url: '/nita_nails.jpg',
+        url: 'https://nita-nails.vercel.app/nita_nails.jpg',
         width: 1200,
         height: 630,
         alt: 'Nita Nails',
@@ -47,12 +47,7 @@ export const metadata: Metadata = {
     title: 'Nita Nails',
     description:
       'Nita Nails - Professzionális műköröm szalon Hódmezővásárhelyen. Széles körű szolgáltatások, egyedi dizájn és kiváló minőség várja vendégeinket. Foglalj időpontot még ma!',
-    images: [
-      {
-        url: '/nita_nails.jpg',
-        alt: 'Nita Nails',
-      },
-    ],
+    images: ['https://nita-nails.vercel.app/nita_nails.jpg'],
   },
 };
 
@@ -65,6 +60,26 @@ export default function RootLayout({
     <html lang='hu' className='h-full antialiased'>
       <head>
         <meta name='apple-mobile-web-app-title' content='NitaNails' />
+        <meta property='og:type' content='website' />
+        <meta property='og:site_name' content='Nita Nails' />
+        <meta property='og:url' content='https://nita-nails.vercel.app/' />
+        <meta property='og:title' content='Nita Nails' />
+        <meta
+          property='og:description'
+          content='Nita Nails - Professzionális műköröm szalon Hódmezővásárhelyen. Széles körű szolgáltatások, egyedi dizájn és kiváló minőség várja vendégeinket. Foglalj időpontot még ma!'
+        />
+        <meta
+          property='og:image'
+          content='https://nita-nails.vercel.app/nita_nails.jpg'
+        />
+        <meta property='og:image:width' content='1200' />
+        <meta property='og:image:height' content='630' />
+        <meta property='og:image:alt' content='Nita Nails' />
+        <meta name='twitter:card' content='summary_large_image' />
+        <meta
+          name='twitter:image'
+          content='https://nita-nails.vercel.app/nita_nails.jpg'
+        />
       </head>
       <body
         className={`${sweety.variable} ${desire.variable} ${vani.variable} bg-white`}
